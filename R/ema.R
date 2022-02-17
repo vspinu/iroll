9ea6aa3190316582a6631929ec55a158bac44f0b
Vitalie Spinu <spinuvit@gmail.com>
2022-02-17 11:49:54 +0100

    Change order of arguments and use consistent argument names

--- R/ema.R
+++ R/ema.R
@@ -1,23 +1,23 @@
 
-cumemaR <- function(x, date, n){
+cumemaR <- function(x, ix = seq_along(x), n){
     N <- length(x)
     ex <- double(N)
     ex[[1]] <- x[[1]]
     for(i in seq_len(max(0, N-1L))){
         i1 <- i + 1L
-        edelta <- exp(- (date[[i1]] - date[[i]])/n)
+        edelta <- exp(- (ix[[i1]] - ix[[i]])/n)
         ex[[i1]] <- ex[[i]]*edelta + x[[i1]]
     }
     ex
 }
 
-emaR <- function(x, date, n){
+emaR <- function(x, ix = seq_along(x), n){
     N <- length(x)
     ex <- double(N)
     ex[[1]] <- x[[1]]
     for(i in seq_len(max(0, N-1L))){
         i1 <- i + 1L
-        W <- exp(-(date[[i1]] - date[[i]])/n)
+        W <- exp(-(ix[[i1]] - ix[[i]])/n)
         ex[[i1]] <- W*ex[[i]] + (1-W)*x[[i1]]
     }
     ex
@@ -28,7 +28,7 @@ emaR <- function(x, date, n){
 ##'
 ##' \code{fwd_xyz} versions are forward looking emas.
 ##' @param x values of the seires
-##' @param date time index
+##' @param ix time index
 ##' @param n ema parameter - number of periods (n = 1/theta where -theta is the
 ##'     exponent in the smoother). At \code{n} periods back \code{x} contributes
 ##'     about 36.8% of the total weight; at \code{3*n} it contributes about 5%
@@ -36,26 +36,26 @@ emaR <- function(x, date, n){
 ##' @param linear boolean, linear or not
 ##' @param cum boolean, cumulative or not
 ##' @export
-ema <- function(x, date, n = 10, linear = F, cum = F) {
+ema <- function(x, ix = seq_along(x), n = 10, linear = F, cum = F) {
     f <-
         if(cum) c_cumema
         else if (linear) c_ema_lin
         else c_ema
-    f(x, date, n)
+    f(x, ix, n)
 }
 
 ##' @export
 ##' @rdname ema
-fwd_ema <- function(x, date, n = 10, linear = F, cum = F) {
-    rev(ema(rev(x), -rev(as.numeric(date)), n, linear, cum))
+fwd_ema <- function(x, ix = seq_along(x), n = 10, linear = F, cum = F) {
+    rev(ema(rev(x), -rev(as.numeric(ix)), n, linear, cum))
 }
 
 ##' @rdname ema
 ##' @param weight weight
 ##' @export
-wema <- function(x, weight, date, n = 10, linear = F, cum = T){
-    num <- ema(x*weight, date, n, linear, cum)
-    den <- ema(weight, date, n, linear, cum)
+wema <- function(x, weight, ix = seq_along(x), n = 10, linear = F, cum = T){
+    num <- ema(x*weight, ix, n, linear, cum)
+    den <- ema(weight, ix, n, linear, cum)
     out <- num/den
     ## avoid infinitity for num==0
     out[num == 0] <- 0
@@ -66,11 +66,11 @@ wema <- function(x, weight, date, n = 10, linear = F, cum = T){
 ##' @param nslow nr of periods for slow moving EMA
 ##' @param nfast nr of periods for fast moving EMA
 ##' @export
-macd <- function(x, date, nfast = 12, nslow = nfast*4, nbase = nfast*3, linear = F, cum = F){
-    fast <- ema(x, date, nfast, linear, cum)
-    slow <- ema(x, date, nslow, linear, cum)
+macd <- function(x, ix = seq_along(x), nfast = 12, nslow = nfast*4, nbase = nfast*3, linear = F, cum = F){
+    fast <- ema(x, ix, nfast, linear, cum)
+    slow <- ema(x, ix, nslow, linear, cum)
     out <- fast/slow - 1
-    ## base <- ema(x, date, nbase, linear, cum)
+    ## base <- ema(x, ix, nbase, linear, cum)
     ## out <- (fast - slow)/base
     out[is.nan(out)] <- 0
     out
@@ -78,9 +78,9 @@ macd <- function(x, date, nfast = 12, nslow = nfast*4, nbase = nfast*3, linear =
 
 ##' @rdname ema
 ##' @export
-wmacd <- function(x, weight, date, nfast = 12, nslow = nfast*4L, linear = F, cum = T){
-    fast <- wema(x, weight, date, nfast, linear, cum)
-    slow <- wema(x, weight, date, nslow, linear, cum)
+wmacd <- function(x, weight, ix = seq_along(x), nfast = 12, nslow = nfast*4L, linear = F, cum = T){
+    fast <- wema(x, weight, ix, nfast, linear, cum)
+    slow <- wema(x, weight, ix, nslow, linear, cum)
     out <- fast/slow - 1
     out[is.nan(out)] <- 0
     out
@@ -88,53 +88,55 @@ wmacd <- function(x, weight, date, nfast = 12, nslow = nfast*4L, linear = F, cum
 
 ##' @rdname ema
 ##' @export
-fwd_macd <- function(x, date, nfast = 12, nslow = nfast*4, linear = F, cum = F) {
-    -rev(macd(rev(x), -rev(as.numeric(date)), nfast, nslow, linear, cum))
+fwd_macd <- function(x, ix = seq_along(x), nfast = 12, nslow = nfast*4, linear = F, cum = F) {
+    -rev(macd(rev(x), -rev(as.numeric(ix)), nfast, nslow, linear, cum))
 }
 
 ##' @rdname ema
 ## emsd: Exponential moving standard deviation.
 ##' @export
-emsd <- function(x, date, n = 10, normalize = T, linear = F, cum = F){
-    emean <- ema(x, date, n, linear, cum)
+emsd <- function(x, ix = seq_along(x), n = 10, normalize = T, linear = F, cum = F){
+    emean <- ema(x, ix, n, linear, cum)
     dev <- (x - emean)^2L
-    if (normalize) dev <- dev/(emean^2 + 0.0001)
-    sqrt(ema(dev, date, n, linear, cum))
+    if (normalize)
+      dev <- dev/(emean^2 + .Machine$double.eps)
+    sqrt(ema(dev, ix, n, linear, cum))
 }
 
 ##' @rdname ema
-fwd_emsd <- function(x, date, n = 10, linear = F, cum = F) {
-    rev(emsd(rev(x), -rev(as.numeric(date)), n, linear, cum))
+fwd_emsd <- function(x, ix = seq_along(x), n = 10, linear = F, cum = F) {
+    rev(emsd(rev(x), -rev(as.numeric(ix)), n, linear, cum))
 }
 
 ##' @rdname ema
 ## emsd: Exponential moving standard deviation.
 ##' @export
-wemsd <- function(x, weight, date, n = 10, normalize = T, linear = F, cum = T){
-    emean <- wema(x, weight, date, n, linear, cum)
+wemsd <- function(x, ix = seq_along(x), weight, n = 10, normalize = T, linear = F, cum = T){
+    emean <- wema(x, ix, weight, n, linear, cum)
     dev <- (x - emean)^2L
-    if (normalize) dev <- dev/(emean^2 + 0.0001)
-    sqrt(wema(dev, weight, date, n, linear, F))
+    if (normalize)
+      dev <- dev/(emean^2 + .Machine$double.eps)
+    sqrt(wema(dev, weight, ix, n, linear, F))
 }
 
 ##' \code{xyz2} functions are two sided version (aka filters).
 ##' @rdname ema
 ##' @export
-ema2 <- function(x, date, n = 10, linear = F, cum = F){
+ema2 <- function(x, ix = seq_along(x), n = 10, linear = F, cum = F){
     f <-
         if(cum) c_cumema
         else if (linear) c_ema_lin
         else c_ema
-    forw <- f(x, date, n)
-    back <- f(rev(x), -rev(as.numeric(date)), n)
+    forw <- f(x, ix, n)
+    back <- f(rev(x), -rev(as.numeric(ix)), n)
     (forw + rev(back))/2
 }
 
 ##' @rdname ema
 ##' @export
-wema2 <- function(x, weight, date, n = 10, linear = F, cum = F){
-    num <- ema2(x*weight, date, n, linear, cum)
-    den <- ema2(weight, date, n, linear, cum)
+wema2 <- function(x, ix = seq_along(x), weight, n = 10, linear = F, cum = F){
+    num <- ema2(x*weight, ix, n, linear, cum)
+    den <- ema2(weight, ix, n, linear, cum)
     out <- num/den
     ## avoid infinitity for num==0
     out[num == 0] <- 0
@@ -143,9 +145,9 @@ wema2 <- function(x, weight, date, n = 10, linear = F, cum = F){
 
 ##' @rdname ema
 ##' @export
-macd2 <- function(x, date, nfast = 12, nslow = nfast*4, linear = F, cum = F){
-    fast <- ema2(x, date, nfast, linear, cum)
-    slow <- ema2(x, date, nslow, linear, cum)
+macd2 <- function(x, ix = seq_along(x), nfast = 12, nslow = nfast*4, linear = F, cum = F){
+    fast <- ema2(x, ix, nfast, linear, cum)
+    slow <- ema2(x, ix, nslow, linear, cum)
     out <- fast/slow - 1
     out[is.nan(out)] <- 0
     out
--- R/iroll.R
+++ R/iroll.R
@@ -16,9 +16,9 @@ NULL
 ##' Rolling function for iregular series
 ##' @rdname iroll
 ##' @export
+##' @param x value to be rolled over
 ##' @param ix index converible to numeric (Date, POSIXct, etc). Must be ordered
 ##'     increasingly.
-##' @param x value to be rolled over
 ##' @param left left bound scalar (convertible to numeric)
 ##' @param right right bound scalar (convertible to numeric)
 ##' @param left_open if the interval is left open
@@ -32,16 +32,16 @@ iroll_mean <- function(ix, x, left, right = 0, left_open = FALSE, right_open = F
 
 ##' @rdname iroll
 ##' @export
-iroll_sd <- function(ix, x, left, right = 0, left_open = FALSE, right_open = FALSE, fill = NA_real_) {
+iroll_sd <- function(x, ix = seq_along(x), left, right = 0, left_open = FALSE, right_open = FALSE, fill = NA_real_) {
   out <- c_roll_sd(as.numeric(ix), x,
-                    as.numeric(left)[1], as.numeric(right)[1],
-                    left_open, right_open, fill)
+                   as.numeric(left)[1], as.numeric(right)[1],
+                   left_open, right_open, fill)
   .reclass(out, x)
 }
 
 ##' @rdname iroll
 ##' @export
-iroll_min <- function(ix, x, left, right = 0, left_open = FALSE, right_open = FALSE, fill = NA_real_) {
+iroll_min <- function(x, ix = seq_along(x), left, right = 0, left_open = FALSE, right_open = FALSE, fill = NA_real_) {
   out <- c_roll_min(as.numeric(ix), x,
                     as.numeric(left)[1], as.numeric(right)[1],
                     left_open, right_open, fill)
@@ -50,7 +50,7 @@ iroll_min <- function(ix, x, left, right = 0, left_open = FALSE, right_open = FA
 
 ##' @rdname iroll
 ##' @export
-iroll_max <- function(ix, x, left, right = 0, left_open = FALSE, right_open = FALSE, fill = NA_real_) {
+iroll_max <- function(x, ix = seq_along(x), left, right = 0, left_open = FALSE, right_open = FALSE, fill = NA_real_) {
   out <- c_roll_max(as.numeric(ix), x,
                     as.numeric(left)[1], as.numeric(right)[1],
                     left_open, right_open, fill)
@@ -59,7 +59,7 @@ iroll_max <- function(ix, x, left, right = 0, left_open = FALSE, right_open = FA
 
 ##' @rdname iroll
 ##' @export
-iroll_first <- function(ix, x, left, right = 0, left_open = FALSE, right_open = FALSE, fill = NA_real_) {
+iroll_first <- function(x, ix = seq_along(x), left, right = 0, left_open = FALSE, right_open = FALSE, fill = NA_real_) {
   out <- c_roll_first(as.numeric(ix), x,
                       as.numeric(left)[1], as.numeric(right)[1],
                       left_open, right_open, fill)
@@ -68,7 +68,7 @@ iroll_first <- function(ix, x, left, right = 0, left_open = FALSE, right_open = 
 
 ##' @rdname iroll
 ##' @export
-iroll_last <- function(ix, x, left, right = 0, left_open = FALSE, right_open = FALSE, fill = NA_real_) {
+iroll_last <- function(x, ix = seq_along(x), left, right = 0, left_open = FALSE, right_open = FALSE, fill = NA_real_) {
   out <- c_roll_last(as.numeric(ix), x,
                      as.numeric(left)[1], as.numeric(right)[1],
                      left_open, right_open, fill)
@@ -78,7 +78,7 @@ iroll_last <- function(ix, x, left, right = 0, left_open = FALSE, right_open = F
 
 ##' @rdname iroll
 ##' @export
-iroll_sum <- function(ix, x, left, right = 0, left_open = FALSE, right_open = FALSE, fill = NA_real_) {
+iroll_sum <- function(x, ix = seq_along(x), left, right = 0, left_open = FALSE, right_open = FALSE, fill = NA_real_) {
   out <- c_roll_sum(as.numeric(ix), x,
                     as.numeric(left)[1], as.numeric(right)[1],
                     left_open, right_open, fill)
@@ -87,7 +87,7 @@ iroll_sum <- function(ix, x, left, right = 0, left_open = FALSE, right_open = FA
 
 ##' @rdname iroll
 ##' @export
-iroll_prod <- function(ix, x, left, right = 0, left_open = FALSE, right_open = FALSE, fill = NA_real_) {
+iroll_prod <- function(x, ix = seq_along(x), left, right = 0, left_open = FALSE, right_open = FALSE, fill = NA_real_) {
   out <- c_roll_prod(as.numeric(ix), x,
                      as.numeric(left)[1], as.numeric(right)[1],
                      left_open, right_open, fill)
@@ -97,7 +97,7 @@ iroll_prod <- function(ix, x, left, right = 0, left_open = FALSE, right_open = F
 ##' @rdname iroll
 ##' @export
 ##' @param prob numeric scalar in [0, 1].
-iroll_quantile <- function(ix, x, left, right = 0,
+iroll_quantile <- function(ix, x = seq_along(x), left, right = 0,
                            left_open = FALSE, right_open = FALSE,
                            prob = .5, fill = NA_real_) {
   out <- c_roll_quantile(as.numeric(ix), x,
